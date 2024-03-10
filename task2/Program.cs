c02807eb30e28a12a57bdd2487e699c7439a2e59
Esmer <bd7tqjs7c@code.edu.az>
2024-03-10 19:08:52 +0400

    loopstask

---
+++ task2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Taks 4
+         // int sum = 0;
+         //   int m = 1331;
+         //   int qaliq =0;
+         //   int ededintersi = 1331;
+         //   while (m>0)
+         //   {
+         //      qaliq = m % 10;
+         //      sum=(sum*10)+qaliq; 
+         //       m=m/10;
+         //    }
+         //    if (ededintersi==sum ) 
+         //    {
+         //       Console.WriteLine(" eded palindromdur");
+         //    } 
+         //    else
+         //   {
+         //       Console.WriteLine("eded palindrom deyil");
+         //  }
+
+
+
+
+
+
+            //Task 3
+            // int n = 5;
+            //   int faktorial = 1;
+            //   for (int i = 1; i <= 5; i++)
+            //  {
+            //     faktorial *= i;
+            // }
+            //   Console.WriteLine (n + "!=" + faktorial);
+
+            //Task 2
+            //int n = 27;
+            //int i = 2;
+            //for (i = 2; i <= 27; i++)
+            //{
+            //    if (n % i == 0)
+            //    {
+            //        Console.WriteLine("murekkeb");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sade");
+
+            //    }
+            //}
+
+            // Task 1
+            //int m = 45;
+            // int a = 4;
+            //int b = 5;
+            // int hasil = a * b;
+            // { 
+            // Console.WriteLine(hasil);
+            // }
+        }
+    }
+}
